4d81c0e4322b8ae176c6954ac231b3835688db4a
Joacir Gomes de Souza Filho <joacir.filho60@aluno.ifce.edu.br>
2025-02-10 14:51:57 -0300

    Add files via upload

--- models/pedido_has_produto.js
+++ models/pedido_has_produto.js
@@ -1,26 +1,57 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class pedido_has_produto extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  class PedidoHasProduto extends Model {
     static associate(models) {
-      // define association here
+      this.belongsTo(models.Pedido, { foreignKey: 'idpedido', as: 'pedido' });
+      this.belongsTo(models.Produto, { foreignKey: 'idproduto', as: 'produto' });
     }
   }
-  pedido_has_produto.init({
-    idpedido: DataTypes.INTEGER,
-    idproduto: DataTypes.INTEGER,
-    quantidade: DataTypes.INTEGER,
-    preco_unitario: DataTypes.FLOAT
-  }, {
-    sequelize,
-    modelName: 'pedido_has_produto',
-  });
-  return pedido_has_produto;
+
+  PedidoHasProduto.init(
+    {
+      idpedido: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'Pedidos', 
+          key: 'id'
+        }
+      },
+      idproduto: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'Produtos',
+          key: 'id'
+        }
+      },
+      quantidade: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+        validate: {
+          min: 1
+        }
+      },
+      preco_unitario: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+          min: 0
+        }
+      }
+    },
+    {
+      sequelize,
+      modelName: 'PedidoHasProduto',
+      tableName: 'pedido_has_produto',
+      timestamps: false
+    }
+  );
+
+  return PedidoHasProduto;
 };

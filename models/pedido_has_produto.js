da33914d338dd7ea42c4aa992eda13b70a67f28c
Joacir Gomes de Souza Filho <joacir.filho60@aluno.ifce.edu.br>
2025-02-11 12:57:51 -0300

    Add files via upload

--- models/pedido_has_produto.js
+++ models/pedido_has_produto.js
@@ -3,51 +3,59 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
+    // tabela intermediária entre Pedido e Produto
     const PedidoHasProduto = sequelize.define('PedidoHasProduto', {
+       
         idpedido: {
             type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
+            allowNull: false, // Não pode ser nulo, pois faz parte da chave primária composta
+            primaryKey: true, // Chave primária composta
             references: {
                 model: 'Pedidos', 
                 key: 'id'
             }
         },
+        
         idproduto: {
             type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
+            allowNull: false, // Não pode ser nulo, pois faz parte da chave primária composta
+            primaryKey: true, // Chave primária composta
             references: {
-                model: 'Produtos',
+                model: 'Produtos', 
                 key: 'id'
             }
         },
+        // Quantidade de um determinado produto dentro de um pedido
         quantidade: {
             type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 1,
+            allowNull: false, 
+            defaultValue: 1, // Se não for informado, assume 1
             validate: {
-                min: 1
+                min: 1 
             }
         },
+        // Preço unitário do produto no momento do pedido
         preco_unitario: {
             type: DataTypes.FLOAT,
-            allowNull: false,
+            allowNull: false, // O preço deve sempre estar definido
             validate: {
-                min: 0
+                min: 0 
             }
         }
     }, {
-        tableName: 'pedido_has_produto',
-        timestamps: false
+        tableName: 'pedido_has_produto', 
+        timestamps: false 
     });
 
+    // Definição das associações com os modelos Pedido e Produto
     PedidoHasProduto.associate = (models) => {  
+        // Associação com Pedido (um pedido pode conter vários produtos)
         PedidoHasProduto.belongsTo(models.Pedido, {
             foreignKey: 'idpedido',
             as: 'pedido'
         });
 
+        // Associação com Produto (um produto pode estar em vários pedidos)
         PedidoHasProduto.belongsTo(models.Produto, {
             foreignKey: 'idproduto',
             as: 'produto'

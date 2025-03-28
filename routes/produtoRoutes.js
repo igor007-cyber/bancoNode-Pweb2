42e88d13bfd2187fbe0510a4ad6865d4f98e3bab
Jadienne1 <jadiennecostasilva@gmail.com>
2025-03-28 15:24:23 -0300

    atualiação em produtoroutes

--- routes/produtoRoutes.js
+++ routes/produtoRoutes.js
@@ -14,15 +14,15 @@ import { authenticationToken } from '../middleware/authMiddleware.js';
 const routerProduto = express.Router();
 
 // Rotas protegidas para produtos
-routerProduto.post('/produtos', authenticationToken, createProduto);
-routerProduto.get('/produtos', authenticationToken, getAllProdutos);
-routerProduto.get('/produtos/:id', authenticationToken, getProdutoById);
+routerProduto.post('/cadastrar', authenticationToken, createProduto);
+routerProduto.get('/listar', getAllProdutos);
+routerProduto.get('/lstar/:id', getProdutoById);
 routerProduto.put('/produtos/:id', authenticationToken, updateProduto);
 routerProduto.delete('/produtos/:id', authenticationToken, deleteProduto);
 
 // Rotas para gerenciamento de estoque
 routerProduto.patch('/produtos/:id/add', authenticationToken, adicionarProduto); // Adiciona quantidade ao estoque
 routerProduto.patch('/produtos/:id/remove', authenticationToken, removerProduto); // Remove quantidade do estoque
-routerProduto.get('/produtos/:id/quantidade', authenticationToken, getQuantidadeProduto); // Obtém a quantidade do produto
+routerProduto.get('/produtos/:id/quantidade', getQuantidadeProduto); // Obtém a quantidade do produto
 
 export default routerProduto;

8dbef8bdb624ee0c4505d79528efd5bac120ea9b
Jadienne1 <jadiennecostasilva@gmail.com>
2025-04-02 14:10:16 -0300

    ajeitando cliente

--- models/cliente.js
+++ models/cliente.js
@@ -1,1 +1,21 @@
-models/cliente.js
+'use strict';
+import { Model}  from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+  class Cliente extends Model {
+    static associate(models) {
+      
+      Cliente.belongsTo(models.Usuario, {
+        foreignKey: 'idUsuario',
+        as: 'usuario'
+      });
+
+      
+      Cliente.hasMany(models.Pedido, {
+        foreignKey: 'idCliente',
+        as: 'pedidos'
+      });
+    }
+  }
+
+  
